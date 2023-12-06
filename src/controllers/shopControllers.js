15678d10b1afd6596b11172d6772edaf3a577926
vadamo85 <valeriaxadamo@gmail.com>
2023-12-06 10:59:48 -0300

    Actualización de mitad de misión 5

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -1,7 +1,7 @@
 const shopControllers = {
     shop: (req, res) => res.render('shop/shop', {layout:'layouts/layout'}),
     shopItem: (req, res) => res.render('shop/item', {layout:'layouts/layout'}),
-    shopAddItem: (req, res) => res.render('admin/create-item',  {layout:'layouts/layout'}),
+    shopAddItem: (req, res) => res.send('Route for add item view'),
     shopCart:(req, res) => res.render('shop/cart', {layout:'layouts/layout'}),
     shopPayCart: (req, res) => res.send('Route for pay the cart View')
 }
