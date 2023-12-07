de40a7758113db406ee13cfb34eeff3b00c0aeaa
firthunandz <fernandonchiesa@gmail.com>
2023-12-07 10:26:27 -0300

    rutas de enlace, rutas del css y logos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 const express = require('express');
 const app = express();
@@ -10,10 +10,10 @@ const authRoutes = require('./src/routes/authRoutes');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './src/views'));
+app.set('views', path.join(__dirname, '/src/views'));
 
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
@@ -23,6 +23,6 @@ app.use('/', shopRoutes);
 app.use('/', adminRoutes);
 app.use('/', authRoutes);
 
-app.listen(4000, () => console.log("Servidor corriendo en http:localhost:4000"));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
 
