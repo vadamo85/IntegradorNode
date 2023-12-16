f23648b33185feb398f9f35943ff6a4b3b210719
MonicaBidart <monica.bidart@gmail.com>
2023-12-16 01:21:32 -0300

    addminCreateItem = Carga de nuevo producto en bbdd -> Hecho, pendiente el tema de imágenes tomar el id/nombre  ||  adminEdit = Edición de producto, status iniciado, pendiente resolver value en selects que no se muestran al querer editar, el resto de los datos los trae. Funciona parte de la edición

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -2,9 +2,10 @@
 const path = require("path");
 const sharp = require("sharp");
 
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
 const model = require("../models/Product");
+const { send } = require("process");
 
 const adminControllers = {
   admin: async (req, res) => {
@@ -22,30 +23,58 @@ const adminControllers = {
     res.render("admin/create", { layout: "layouts/adminLayout" });
   },
 
-  adminCreateItem: (req, res) => {
+  adminCreateItem: async (req, res) => {
     console.log(req.body, req.file);
 
     const errors = validationResult(req);
     // chequea si la request viene con errores
     if (!errors.isEmpty()) {
-        return res.render("admin/create", {
-            values: req.body,
-            errors: errors.array(),
-        });
+      return res.render("admin/create", {
+        values: req.body,
+        errors: errors.array(),
+      });
     }
 
-    if (req.file) {
-      // optimizo el nombre de la imagen:
-      const name = req.file.originalname;
-      sharp(req.file.buffer)
-        .resize(300)
-        .toFile(path.resolve(__dirname, "../../public/uploads/", name)); // toFile requiere una ruta absoluta
-    }
+    // Guardamos la información en la BBDD
+    try {
+      const producto = await model.create(req.body);
+      if (producto && req.file) {
+        // optimizo el nombre de la imagen:
+        const name = req.file.originalname;
+        sharp(req.file.buffer)
+          .resize(300)
+          .toFile(path.resolve(__dirname, "../../public/uploads/", name)); // toFile requiere una ruta absoluta
+      }
 
-    res.send("Crear producto...");
+      res.redirect("/admin");
+    } catch (error) {
+      console.log("ERROR: " + error);
+      res.status(500).send(error);
+    }
   },
-  adminEdit: (req, res) =>
-    res.render("admin/edit-item", { layout: "layouts/adminLayout" }),
+
+  adminEdit: async (req, res) => {
+    try {
+      const producto = await model.findByPk(req.params.id);
+      console.log("PRODUCTOOOOOOO" + producto);
+
+      // Verificar que exista el objeto
+      if (producto) {
+        console.log(producto.category + "----" + producto.licence);
+        res.render("admin/edit-item", {
+          values: producto,
+          layout: "layouts/adminLayout",
+        });
+      } else {
+        // Ideal hacer una vista que diga el producto no existe
+        res.status(404).send("El producto no existe");
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500), send(error);
+    }
+  },
+
   adminEditOK: (req, res) =>
     res.send("Route to confirm changes in an item View"),
   adminDelete: (req, res) => res.send("Route to delete an item"),
